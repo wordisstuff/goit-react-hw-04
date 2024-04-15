105ceb19b61a69e86cd01a44953c035747781501
wordisstuff <wordisstuff@gmail.com>
2024-04-15 02:21:26 -0500

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import usePhotosSearch from "./hooks/usePhotosSearch";
@@ -43,7 +42,6 @@ function App() {
       ) : (
         <ImageGallery photos={photos} openModal={openModal} />
       )}
-
       {photos && page < totalPages && <LoadMoreBtn pageChange={loadMorePage} />}
       <ImageModal
         modalImg={modalImg}
